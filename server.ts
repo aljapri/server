982b03c47a959f11a7047a4c2b87f82a2b1cb223
kali <gamer2mohamad@gmail.com>
2024-03-27 09:01:07 +0300

    update on server

--- server.ts
+++ server.ts
@@ -2,9 +2,9 @@ const express = require("express");
 
 const app = express();
 
+let fsfasf = "fucking";
+let fucking = "sex";
 const one = "hello";
 
-
 console.log(one);
-
-console.log("hello what is your fucking name")
+console.log("live is hard but not in that goood");
